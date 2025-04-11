257d2100e3af622dd407cf3e94f8eb36099c6d60
arkchive <kyungminkim7@konkuk.ac.kr>
2025-04-12 01:34:31 +0900

    프로젝트 생성

---
+++ src/main/java/com/togedy/togedy_server_v2/TogedyServerV2Application.java
@@ -0,0 +1,13 @@
+package com.togedy.togedy_server_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogedyServerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogedyServerV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/togedy/togedy_server_v2/TogedyServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togedy.togedy_server_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogedyServerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
