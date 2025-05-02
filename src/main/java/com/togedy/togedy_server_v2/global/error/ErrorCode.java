cbf144f3193df27cc36954de4a1b26518d1eb891
You-Hyuk <cxz6411@naver.com>
2025-05-02 17:08:36 +0900

    [FEAT/#17] GlobalExceptionHandler 구현

--- src/main/java/com/togedy/togedy_server_v2/global/error/ErrorCode.java
+++ src/main/java/com/togedy/togedy_server_v2/global/error/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // GLOBAL
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G500", "서버 내부에 문제가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G500", "서버 내부에 문제가 발생했습니다."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "G400", "올바르지 않은 값 또는 형식입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
