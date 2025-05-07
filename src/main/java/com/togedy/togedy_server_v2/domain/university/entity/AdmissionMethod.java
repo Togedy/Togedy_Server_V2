342e05a7556768f1525a798f556d60f336b1a96f
You-Hyuk <cxz6411@naver.com>
2025-05-07 23:53:59 +0900

    [FEAT/#20] 대학 일정 엔티티 연관관계 매핑

--- src/main/java/com/togedy/togedy_server_v2/domain/university/entity/AdmissionMethod.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/university/entity/AdmissionMethod.java
@@ -2,9 +2,12 @@ package com.togedy.togedy_server_v2.domain.university.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -25,6 +28,10 @@ public class AdmissionMethod {
 
     @Column(name = "admission_method_name")
     private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "university_id")
+    private University university;
 
     @Builder
     private AdmissionMethod(String name) {
--- src/main/java/com/togedy/togedy_server_v2/domain/university/entity/UniversitySchedule.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/university/entity/UniversitySchedule.java
@@ -2,9 +2,12 @@ package com.togedy.togedy_server_v2.domain.university.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -32,6 +35,10 @@ public class UniversitySchedule {
 
     @Column(name = "end_date")
     private LocalDateTime endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admission_method_id")
+    private AdmissionMethod admissionMethod;
 
     @Builder
     private UniversitySchedule(String admissionStage,
