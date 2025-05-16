050e61f112730ec662a872cb4b090f726cc5f48d
You-Hyuk <cxz6411@naver.com>
2025-05-16 22:56:45 +0900

    [REFACTOR/#20] AdmissionSchedule 중간 테이블 생성

--- src/main/java/com/togedy/togedy_server_v2/domain/university/entity/AdmissionSchedule.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/university/entity/AdmissionSchedule.java
@@ -1,6 +1,5 @@
 package com.togedy.togedy_server_v2.domain.university.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,41 +8,32 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Immutable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name = "admission_method",
+@Table(name = "admission_schedule",
         uniqueConstraints = @UniqueConstraint(
-        columnNames = {"university_id",
-                "admission_method_name"}
+                columnNames = {"admission_methoid_id", "university_schedule_id"}
         )
 )
-@Immutable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AdmissionMethod {
+public class AdmissionSchedule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "admission_method_id")
+    @Column(name = "admission_schedule_id")
     private Long id;
 
-    @Column(name = "admission_method_name", nullable = false)
-    private String name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "admission_method_id", nullable = false, updatable = false)
+    private AdmissionMethod admissionMethod;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "university_id", nullable = false, updatable = false)
-    private University university;
-
-    @OneToMany(mappedBy = "admissionMethod", orphanRemoval = true, cascade = CascadeType.ALL)
-    private List<AdmissionSchedule> admissionScheduleList = new ArrayList<>();
+    @JoinColumn(name = "university_schedule_id", nullable = false, updatable = false)
+    private UniversitySchedule universitySchedule;
 }
--- src/main/java/com/togedy/togedy_server_v2/domain/university/entity/UniversitySchedule.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/university/entity/UniversitySchedule.java
@@ -1,13 +1,12 @@
 package com.togedy.togedy_server_v2.domain.university.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -15,6 +14,8 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Immutable;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "university_schedule")
@@ -40,8 +41,7 @@ public class UniversitySchedule {
     @Column(name = "academic_year", columnDefinition = "YEAR", nullable = false, updatable = false)
     private int academicYear;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "admission_method_id", nullable = false, updatable = false)
-    private AdmissionMethod admissionMethod;
+    @OneToMany(mappedBy = "universitySchedule", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<AdmissionSchedule> admissionScheduleList = new ArrayList<>();
 
 }
