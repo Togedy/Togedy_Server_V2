a8f2262536bc7f2c02a051f0fd517acee35a14c1
You-Hyuk <cxz6411@naver.com>
2025-05-18 03:48:46 +0900

    [FEAT/#20] 일정 비교 메소드 구체화

--- src/main/java/com/togedy/togedy_server_v2/global/util/DateTimeUtils.java
+++ src/main/java/com/togedy/togedy_server_v2/global/util/DateTimeUtils.java
@@ -1,17 +1,34 @@
 package com.togedy.togedy_server_v2.global.util;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Objects;
 
 public class DateTimeUtils {
 
-    public static long durationInSeconds(LocalDateTime startDate, LocalDateTime endDate) {
-        if (startDate == null) {
-            return 0L;
-        }
+    public static long durationInSeconds(
+            LocalDate startDate, LocalTime startTime,
+            LocalDate endDate, LocalTime endTime
+    ) {
         if (endDate == null) {
             return 0L;
         }
-        return Duration.between(startDate, endDate).getSeconds();
+        LocalDateTime start = startDate.atStartOfDay();
+        if (startTime != null) {
+            start = LocalDateTime.of(startDate, startTime);
+        }
+
+        LocalDateTime end = endDate.atStartOfDay();
+        if (endTime != null) {
+            end = LocalDateTime.of(endDate, endTime);
+        }
+
+        return Duration.between(start, end).getSeconds();
+    }
+
+    public static LocalDateTime toStartDateTime(LocalDate date, LocalTime time) {
+        return LocalDateTime.of(date, Objects.requireNonNullElse(time, LocalTime.MIN));
     }
 }
