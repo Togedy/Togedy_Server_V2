31aa3847d1fc5c574932dd124f93138737f3d633
arkchive <kyungminkim7@konkuk.ac.kr>
2025-08-06 16:35:25 +0900

    [FIX/#24] dto 생성자 추가

--- src/main/java/com/togedy/togedy_server_v2/domain/user/dto/TokenRequest.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/user/dto/TokenRequest.java
@@ -1,8 +1,10 @@
 package com.togedy.togedy_server_v2.domain.user.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class TokenRequest {
     private String refreshToken;
 }
