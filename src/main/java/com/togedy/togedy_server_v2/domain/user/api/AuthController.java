b1dd6324674835b8d32de3fbf3669348cb851c95
arkchive <kyungminkim7@konkuk.ac.kr>
2025-08-09 16:39:02 +0900

    [FIX/#24] reissue API에서 토큰을 헤더에서 받도록 수정

--- src/main/java/com/togedy/togedy_server_v2/domain/user/api/AuthController.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/user/api/AuthController.java
@@ -13,10 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,8 +40,8 @@ public class AuthController {
             
             """)
     @PostMapping("/reissue")
-    public ApiResponse<JwtTokenInfo> reissue(@RequestBody TokenRequest request) {
-        return ApiUtil.success(authService.reissueToken(request.getRefreshToken()));
+    public ApiResponse<JwtTokenInfo> reissue(@RequestHeader("Refresh-Token") String refreshToken) {
+        return ApiUtil.success(authService.reissueToken(refreshToken));
     }
 
     @Operation(summary = "로그아웃", description = """
