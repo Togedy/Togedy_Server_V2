2849de996c00ab429606784d160c9ee621725b81
You-Hyuk <cxz6411@naver.com>
2025-08-30 18:58:50 +0900

    [FEAT/#63] S3 기본설정 구현

---
+++ src/main/java/com/togedy/togedy_server_v2/global/service/S3Service.java
@@ -0,0 +1,51 @@
+package com.togedy.togedy_server_v2.global.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.togedy.togedy_server_v2.global.exception.StorageUploadFailedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3Client amazonS3Client;
+
+    public String uploadFile(MultipartFile multipartFile) {
+        String key = createFileName(multipartFile.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize());
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            amazonS3Client.putObject(new PutObjectRequest(bucket, key, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e){
+            throw new StorageUploadFailedException();
+        }
+
+        return amazonS3Client.getUrl(bucket, key).toExternalForm();
+    }
+
+    public void deleteFile(String fileName){
+        amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, fileName));
+    }
+
+    private String createFileName(String fileName){
+        return UUID.randomUUID().toString().concat(fileName);
+    }
+
+}
