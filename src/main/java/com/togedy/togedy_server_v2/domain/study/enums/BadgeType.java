db2a26b54bc994345cba62a25945fff05f7cf47b
You-Hyuk <cxz6411@naver.com>
2025-09-05 17:44:18 +0900

    [FEAT/#65] BadgeType Enum 클래스 생성

--- src/main/java/com/togedy/togedy_server_v2/domain/study/enums/BadgeType.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/study/enums/BadgeType.java
@@ -3,6 +3,5 @@ package com.togedy.togedy_server_v2.domain.study.enums;
 import lombok.Getter;
 
 @Getter
-public enum StudyRole {
-    LEADER, MEMBER
+public enum BadgeType {
 }
--- src/main/java/com/togedy/togedy_server_v2/domain/study/enums/StudyTier.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/study/enums/StudyTier.java
@@ -1,4 +1,7 @@
 package com.togedy.togedy_server_v2.domain.study.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum StudyTier {
 }
