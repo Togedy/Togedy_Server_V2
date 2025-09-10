f583a040b8cfe4d4d8fa8f2295b6fbb959b4c9ce
You-Hyuk <cxz6411@naver.com>
2025-09-10 17:21:03 +0900

    [DOCS/#67] 스터디 API Swagger 문서화

--- src/main/java/com/togedy/togedy_server_v2/domain/study/api/StudyController.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/study/api/StudyController.java
@@ -13,6 +13,8 @@ import com.togedy.togedy_server_v2.domain.study.dto.PostStudyRequest;
 import com.togedy.togedy_server_v2.global.response.ApiResponse;
 import com.togedy.togedy_server_v2.global.security.AuthUser;
 import com.togedy.togedy_server_v2.global.util.ApiUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -28,12 +30,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v2")
 @RequiredArgsConstructor
+@RequestMapping("/api/v2")
+@Tag(name = "Study", description = "스터디 API")
 public class StudyController {
 
     private final StudyService studyService;
 
+    @Operation(summary = "스터디 생성", description = "스터디를 생성한다.")
     @PostMapping(value = "/studies", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponse<Void> createStudy(@ModelAttribute PostStudyRequest request,
                                          @AuthenticationPrincipal AuthUser user) {
@@ -41,6 +45,7 @@ public class StudyController {
         return ApiUtil.successOnly();
     }
 
+    @Operation(summary = "스터디 입장", description = "스터디에 입장한다.")
     @PostMapping("/studies/{studyId}/members")
     public ApiResponse<Void> createStudyMember(
             @RequestBody(required = false) PostStudyMemberRequest request,
@@ -52,6 +57,7 @@ public class StudyController {
         return ApiUtil.successOnly();
     }
 
+    @Operation(summary = "스터디 초대 코드 입장", description = "스터디 초대 코드를 통해 입장한다.")
     @PostMapping("/studies/invitations")
     public ApiResponse<PostStudyInvitationResponse> createStudyInvitation(
             @RequestBody PostStudyInvitationRequest request,
@@ -62,12 +68,14 @@ public class StudyController {
         return ApiUtil.success(response);
     }
 
+    @Operation(summary = "스터디 정보 조회", description = "스터디 정보를 단일 조회한다.")
     @GetMapping("/studies/{studyId}")
     public ApiResponse<GetStudyResponse> readStudy(@PathVariable Long studyId) {
         GetStudyResponse response = studyService.findStudy(studyId);
         return ApiUtil.success(response);
     }
 
+    @Operation(summary = "스터디 이름 중복 검사", description = "스터디 생성 시 이름을 중복 검사한다.")
     @GetMapping("/studies/duplicate")
     public ApiResponse<GetStudyNameDuplicateResponse> readStudyNameDuplicate(
             @RequestParam("name") String name
@@ -77,12 +85,14 @@ public class StudyController {
         return ApiUtil.success(response);
     }
 
+    @Operation(summary = "스터디 초대 코드 조회", description = "해당 스터디의 초대 코드를 조회한다.")
     @GetMapping("/studies/{studyId}/invitation-code")
     public ApiResponse<GetStudyInvitationCodeResponse> readStudyInvitationCode(@PathVariable Long studyId) {
         GetStudyInvitationCodeResponse response = studyService.findStudyInvitationCde(studyId);
         return ApiUtil.success(response);
     }
 
+    @Operation(summary = "스터디 정보 수정", description = "스터디 정보를 수정한다.")
     @PatchMapping(value = "/studies/{studyId}/information", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponse<Void> updateStudyInfo(
             @PathVariable Long studyId,
@@ -93,6 +103,7 @@ public class StudyController {
         return ApiUtil.successOnly();
     }
 
+    @Operation(summary = "스터디 최대 인원 수정", description = "스터디의 최대 인원을 수정한다.")
     @PatchMapping("/studies/{studyId}/members/limit")
     public ApiResponse<Void> updateStudyMemberLimit(
             @PathVariable Long studyId,
@@ -104,6 +115,7 @@ public class StudyController {
         return ApiUtil.successOnly();
     }
 
+    @Operation(summary = "스터디 리더 변경.", description = "스터디 리더를 변경한다.")
     @PatchMapping("/studies/{studyId}/members/{userId}/leader")
     public ApiResponse<Void> updateStudyRole(
             @PathVariable Long studyId,
@@ -115,6 +127,7 @@ public class StudyController {
         return ApiUtil.successOnly();
     }
 
+    @Operation(summary = "스터디 제거", description = "해당 스터디를 제거한다.")
     @DeleteMapping("/studies/{studyId}")
     public ApiResponse<Void> deleteStudy(@PathVariable Long studyId,
                                          @AuthenticationPrincipal AuthUser user) {
@@ -122,6 +135,7 @@ public class StudyController {
         return ApiUtil.successOnly();
     }
 
+    @Operation(summary = "스터디 퇴장", description = "해당 스터디에서 퇴장한다.")
     @DeleteMapping("/studies/{studyId}/members/me")
     public ApiResponse<Void> deleteMyStudyMembership(@PathVariable Long studyId,
                                                      @AuthenticationPrincipal AuthUser user) {
@@ -129,6 +143,7 @@ public class StudyController {
         return ApiUtil.successOnly();
     }
 
+    @Operation(summary = "스터디 추방", description = "스터디에서 해당 유저를 추방한다.")
     @DeleteMapping("/studies/{studyId}/members/{userId}")
     public ApiResponse<Void> deleteStudyMember(
             @PathVariable Long studyId,
