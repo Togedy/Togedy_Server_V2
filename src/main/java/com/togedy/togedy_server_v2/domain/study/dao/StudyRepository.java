ef15c88d0dd851ab8ff23f67ed805485c1de4bbc
You-Hyuk <cxz6411@naver.com>
2025-09-19 17:28:41 +0900

    [REMOVE/#70] 스터디 초대 코드 메소드 삭제

--- src/main/java/com/togedy/togedy_server_v2/domain/study/dao/StudyRepository.java
+++ src/main/java/com/togedy/togedy_server_v2/domain/study/dao/StudyRepository.java
@@ -3,12 +3,8 @@ package com.togedy.togedy_server_v2.domain.study.dao;
 import com.togedy.togedy_server_v2.domain.study.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface StudyRepository extends JpaRepository<Study, Long> {
-    boolean existsByInvitationCode(String invitationCode);
 
     boolean existsByName(String studyName);
 
-    Optional<Study> findByInvitationCode(String invitationCode);
 }
