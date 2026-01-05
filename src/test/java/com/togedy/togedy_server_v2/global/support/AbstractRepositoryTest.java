7918ffd8674bf0f6bef951d1fe407d1c4281c871
You-Hyuk <cxz6411@naver.com>
2026-01-05 11:09:59 +0900

    [FIX/#95] RepositoryTest 테스트 컨테이너 연결 오류 수정

--- src/test/java/com/togedy/togedy_server_v2/global/support/AbstractRepositoryTest.java
+++ src/test/java/com/togedy/togedy_server_v2/global/support/AbstractRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.togedy.togedy_server_v2.global.support;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-public abstract class AbstractRepositoryTest extends AbstractInfraTest {
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public abstract class AbstractRepositoryTest {
 }
