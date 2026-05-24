bcc9d51779a280a2180603a2d94f4fd729fe0259
arkchive <kyungminkim7@konkuk.ac.kr>
2026-05-24 21:30:25 +0900

    [feat/#165] 전역 타임존 설정

--- src/main/java/com/togedy/togedy_server_v2/TogedyServerV2Application.java
+++ src/main/java/com/togedy/togedy_server_v2/TogedyServerV2Application.java
@@ -1,5 +1,6 @@
 package com.togedy.togedy_server_v2;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TogedyServerV2Application {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(TogedyServerV2Application.class, args);
     }
 
